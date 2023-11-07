b8b4d90372e83c02531a3036519c48cbccd5a3f6
Mohammed Arshad <mohammedarshad62820@gmail.com>
2023-11-07 19:03:03 +0530

    placeholder changed

--- w-1/src/components/Header/Spicy.tsx
+++ w-1/src/components/Header/Spicy.tsx
@@ -8,7 +8,7 @@ function Spicy() {
     <>
     <div className='h- bg-cover mt-8 ' style={{backgroundImage:`url(https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg)`}}>
         <div className='  p-28 flex items-center justify-center gap-10 md:flex  flex-col md:p-48 md:gap-10 '>
-        <input className='w-[50%] h-9 border-2 border-black rounded-full text-center' type="text" placeholder='Enter Your Email' />
+        <input className='w-[50%] h-9 border-2 border-black rounded-full text-center' type="text" placeholder='Enter Your palli' />
         <input className='w-[50%] h-9 border-2 border-black rounded-full text-center' type="text " placeholder='Password' />
         <button className='border-2 w-[20%] h-9 font-bold bg-red-500  border-white rounded-full hover:bg-black hover:text-white '>LOG IN</button>
         </div>
